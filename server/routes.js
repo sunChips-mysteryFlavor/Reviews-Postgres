28529fd03c2d7f734bde1daea3ad2f320498a658
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-02-23 17:19:33 -0800

    fixing routes

--- server/routes.js
+++ server/routes.js
@@ -2,17 +2,17 @@ const ctrl = require('./controllers');
 const router = require('express').Router();
 
 //get all reviews
-router.get('/reviews', ctrl.reviews.get); // WORKING
+router.get('/', ctrl.reviews.get); // WORKING
 
 //post a new review
-router.post('/reviews', ctrl.reviews.post); // WORKING
+router.post('/', ctrl.reviews.post); // WORKING
 
 //update the helpful or reported status of a review
-router.put('/reviews/:review_id/helpful', ctrl.reviews.put); // WORKING
-router.put('/reviews/:review_id/report', ctrl.reviews.remove);
+router.put('/:review_id/helpful', ctrl.reviews.put); // WORKING
+router.put('/:review_id/report', ctrl.reviews.remove);
 
 //get review meta data
-router.get('/reviews/meta', ctrl.reviewMeta.get); // WORKING
+router.get('/meta', ctrl.reviewMeta.get); // WORKING
 
 
 module.exports = router;
